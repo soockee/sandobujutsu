1938c753f0d1c6749a0d847433c729a973df537f
Simon Stockhause <simon.stockhause@igd.fraunhofer.de~>
2020-08-20 15:47:50 +0200

    themecolor

--- gatsby/src/components/StartBlock.js
+++ gatsby/src/components/StartBlock.js
@@ -5,7 +5,7 @@ export const StartBlock = withMenuComponentBlock({
   id: 'about',
   Component: () => <>
     <div className='columns'>
-      <div className='column is-7' />
+      <div className='column is-2' />
       <div className='column is-4 has-text-centered is-size-3-desktop is-size-4-touch'>
         <div className='brand-name brand-name-900'>Sando Bu Jutsu</div>
         <div className='brand-name brand-name-400'>Die Samurai-Schule der Drei Wege</div>
