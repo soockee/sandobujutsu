bef9dc70d9fc95c833c30e1b4b0a285b62fdabcd
Simon Stockhause <simon.stockhause@igd.fraunhofer.de~>
2020-08-20 14:56:42 +0200

    background

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -3,6 +3,8 @@ import { Helmet } from 'react-helmet';
 import { ClientsBlock } from '../components/ClientsBlock';
 import { ContactsBlock } from '../components/ContactsBlock';
 import { InfoBlock } from '../components/InfoBlock';
+import { HoursBlock } from '../components/HoursBlock';
+
 import { Menu } from '../components/Menu';
 import { StartBlock } from '../components/StartBlock';
 import '../styles/index.scss';
